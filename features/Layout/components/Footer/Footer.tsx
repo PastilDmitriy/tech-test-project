fcf7d35e5c393810814eb834f7c17f5b5f7e9090
Dmytro Pastil <dpastil@gmail.com>
2026-02-19 12:43:36 +0100

    Added layout and ui library

---
+++ features/Layout/components/Footer/Footer.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer footer-center border-t border-base-300 bg-base-200 p-10 text-base-content">
+      <aside className="grid-flow-col items-center gap-4">
+        <div className="grid-flow-col gap-4">
+          <Link href="/" className="link-hover link font-bold">
+            About
+          </Link>
+          <Link href="/" className="link-hover link">
+            Contact
+          </Link>
+          <Link href="/" className="link-hover link">
+            Careers
+          </Link>
+          <Link href="/" className="link-hover link">
+            FAQ
+          </Link>
+        </div>
+      </aside>
+      <aside className="grid-flow-col items-center gap-4">
+        <p className="font-bold">Questplay</p>
+        <p>Your gaming destination.</p>
+        <p>© {currentYear} Questplay. All rights reserved.</p>
+      </aside>
+      <aside>
+        <p className="footer-title">Newsletter</p>
+        <div className="form-control w-80">
+          <label className="label">
+            <span className="label-text">
+              Stay updated with our latest news
+            </span>
+          </label>
+          <div className="join w-full">
+            <input
+              type="email"
+              placeholder="email@example.com"
+              className="input join-item input-bordered w-full"
+            />
+            <button type="button" className="btn join-item btn-primary">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </aside>
+    </footer>
+  );
+};
